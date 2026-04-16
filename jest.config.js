076ea99005b2fa906871a6fe53cbb1b8e4b822a1
Jacopo Labardi <2527836+labodj@users.noreply.github.com>
2026-04-16 22:11:49 +0200

    chore(release): prepare v1.5.2

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  roots: ["<rootDir>/src"],
   testMatch: ["**/__tests__/**/*.test.ts"],
   coveragePathIgnorePatterns: ["/src/__tests__/helpers/"],
 };
